0669255740c94ada3791c901ac862d1f36eae187
Chi-chi Wang <chichi@wangchiyi.com>
2016-05-05 19:26:09 -0500

    [book show] Pass dynamic page show test

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,4 +2,8 @@ class BooksController < ApplicationController
   def index
     @books = Book.unscoped
   end
+
+  def show
+    @book = Book.find(params[:id])
+  end
 end
--- spec/books/features/user-visits-a-book-page_spec.rb
+++ spec/books/features/user-visits-a-book-page_spec.rb
@@ -1,15 +1,14 @@
 require "rails_helper"
 
 RSpec.describe "User visits a book page", :type => :feature do
-  let(:book1) { create(:book, title: "A Tale of Two Cities") }
-  let(:book2) { create(:book, title: "Huckleberry Finn") }
+  let!(:book1) { create(:book, title: "A Tale of Two Cities") }
+  let!(:book2) { create(:book, title: "Huckleberry Finn") }
 
   it "provides details for a single book" do
     visit root_path
-
     click_link "Books"
-
     click_link(book2.title)
+
     expect(page).to have_content(book2.title)
   end
 end
