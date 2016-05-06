237cc028ea1dbe32f6a82f381bf45ce6998d059f
Chi-chi Wang <chichi@wangchiyi.com>
2016-05-05 19:14:45 -0500

    [show book test] Dynamic book page test

--- spec/books/features/user-visits-a-book-page_spec.rb
+++ spec/books/features/user-visits-a-book-page_spec.rb
@@ -1,15 +1,16 @@
 require "rails_helper"
 
 RSpec.describe "User visits a book page", :type => :feature do
+  let(:book1) { create(:book, title: "A Tale of Two Cities") }
+  let(:book2) { create(:book, title: "Huckleberry Finn") }
+
   it "provides details for a single book" do
-    create(:book, title: "A Tale of Two Cities")
     visit root_path
 
     click_link "Books"
 
-    click_link "A Tale of Two Cities"
-
-    expect(page).to have_content "A Tale of Two Cities"
+    click_link(book2.title)
+    expect(page).to have_content(book2.title)
   end
 end
 
