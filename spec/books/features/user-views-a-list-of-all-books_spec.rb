39ac3b22608032cc63014b4ce401ae840377ae3c
Chi-chi Wang <chichi@wangchiyi.com>
2016-05-05 19:13:45 -0500

    [books index] Refactor and pass dynamic content test

--- spec/books/features/user-views-a-list-of-all-books_spec.rb
+++ spec/books/features/user-views-a-list-of-all-books_spec.rb
@@ -1,13 +1,12 @@
 require "rails_helper"
 
 RSpec.describe "User views a list of all books", :type => :feature do
+  let!(:book1) { create(:book, title: "A Tale of Two Cities") }
+  let!(:book2) { create(:book, title: "Huckleberry Finn") }
+
   it "displays a list of all books" do
-    create(:book, title: "A Tale of Two Cities")
-    create(:book, title: "Huckleberry Finn")
-
     visit books_path
-
-    expect(page).to have_content "A Tale of Two Cities"
-    expect(page).to have_content "Huckleberry Finn"
+    expect(page).to have_link(book1.title, href: "/books/#{book1.id}")
+    expect(page).to have_link(book2.title, href: "/books/#{book2.id}")
   end
 end
