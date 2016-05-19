9e40d5230d8adfca6e4c69122172e2bc88e7fa7e
Chi-chi Wang <chichi@wangchiyi.com>
2016-05-18 19:55:54 -0500

    [genres] Failing test: visit genres_path

--- spec/genres/user-views-a-list-of-all-genres_spec.rb
+++ spec/genres/user-views-a-list-of-all-genres_spec.rb
@@ -7,5 +7,7 @@ RSpec.describe "User views a list of all genres", :type => :feature do
   it "displays a list of all genres" do
     genre1 = create_genre(name: 'Western')
     genre2 = create_genre(name: 'Science Fiction')
+
+    visit genres_path
   end
 end
