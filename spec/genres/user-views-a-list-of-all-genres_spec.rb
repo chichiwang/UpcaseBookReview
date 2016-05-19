1285189399dcbcfa6dff1bd4d6b5e79de4333ffe
Chi-chi Wang <chichi@wangchiyi.com>
2016-05-18 20:01:25 -0500

    [genres index] Failing test: expect links

--- spec/genres/user-views-a-list-of-all-genres_spec.rb
+++ spec/genres/user-views-a-list-of-all-genres_spec.rb
@@ -9,5 +9,8 @@ RSpec.describe "User views a list of all genres", :type => :feature do
     genre2 = create_genre(name: 'Science Fiction')
 
     visit genres_path
+
+    expect(page).to have_link(genre1.name, href: '/genres/#{genre1.id}')
+    expect(page).to have_link(genre2.name, href: '/genres/#{genre2.id}')
   end
 end
