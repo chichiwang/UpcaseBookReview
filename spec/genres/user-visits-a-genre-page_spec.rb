b74cd278f95caeefb14bb78ab2048ab8b4ef0707
Chi-chi Wang <chichi@wangchiyi.com>
2016-06-08 18:25:01 -0500

    [genre] Add spec for user visits a genre page   Failing test: genre model missing method description=

---
+++ spec/genres/user-visits-a-genre-page_spec.rb
@@ -0,0 +1,21 @@
+require "rails_helper"
+
+RSpec.describe "User visits a genre page", :type => :feature do
+  it "lists all the books in the given genre" do
+    genre_details = {
+      name: "Vampire Bromance",
+      description: "Think \"From Dusk Til Dawn\" meets \"Boondock Saints\""
+    }
+    genre = create_genre(genre_details)
+
+    book = create_book( title: "Twi-lizzle", genre: "Vampire Bromance")
+
+    visit root_path
+    click_link "Genres"
+    click_link(genre.name)
+
+    expect(page).to have_content(genre.name)
+    expect(page).to have_link(book.name, href: book_path(book.id))
+  end
+end
+
