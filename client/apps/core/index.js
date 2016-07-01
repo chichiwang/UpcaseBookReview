f0ea5722a9c59660cf21e1fb8796a322cceb8335
Paul Haddad <paulh16@gmail.com>
2016-06-30 19:42:49 -0500

    Add page content container

--- client/apps/core/index.js
+++ client/apps/core/index.js
@@ -4,5 +4,5 @@ import $header from 'modules/header';
 const $body = window.document.body;
 El.setAttribute($body, "style", "margin: 0;");
 
-const $wrapper = window.document.querySelector(".content-wrapper");
-El.renderInto($wrapper, $header.create());
+const $wrapper = window.document.querySelector(".page-wrapper");
+El.prependTo($wrapper, $header.create());
